3dc6ca0955dfea377210b4c88571de3fcb4f3769
Renal24 <renalmukanaev474@gmail.com>
2026-01-14 18:20:08 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
+
+// Растягиваем холст на весь экран
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Твой код для рисования начнется здесь
+console.log("Холст готов к работе!");
